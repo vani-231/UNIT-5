6324182adfd01c832e530f52e7fac4b17d1343aa
vani-231 <vani630145@gmail.com>
2021-12-24 13:54:02 +0530

    .

--- UNIT-5-CODING/PRACTICE/22_12_2021/src/index.js
+++ UNIT-5-CODING/PRACTICE/22_12_2021/src/index.js
@@ -2,6 +2,9 @@
 
 import("./index.css")
 
+import React from "react";
+
+import ReactDom from "react-dom";
 
 function add(a,b){
     return a+b
@@ -11,13 +14,20 @@ console.log(add(2,8));
 
 console.log(add(4,8));
 
-const h1 =  document.createElement("h1")
+// const h1 =  document.createElement("h1")
 
-h1.textContent = "Hello webpack!"
-
-
+// h1.textContent = "Hello webpack!"
 
 
-h1.classList.add("hidden") 
-document.getElementById("root").appendChild(h1)
+
+
+// h1.classList.add("hidden") 
+// document.getElementById("root").appendChild(h1)
+
+ReactDom.render(
+    React.createElement("h1",{className:"hidden"},"HELLO WORLD!"),
+    document.getElementById("root")
+)
+
+
 
