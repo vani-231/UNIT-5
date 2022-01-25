f63227f47485a4ca847cf3be86759d07bd2a6982
vani-231 <vani630145@gmail.com>
2022-01-25 20:19:00 +0530

    starting nextjs

---
+++ NEXT-JS/starting/pages/index.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <div style={{ color: "green", fontSize: "60px" }} >
+        Starting NEXT JS App
+        Hello world!!
+      </div>
+    </>
+  )
+}
